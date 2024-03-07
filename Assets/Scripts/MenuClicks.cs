2f37d4d196d9f47abd614476be9c372ef7317f76
shiftyLogm <poisondroid456@gmail.com>
2024-03-07 06:21:07 -0300

    07/03 PT1

--- Assets/Scripts/MenuClicks.cs
+++ Assets/Scripts/MenuClicks.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MenuClicks : MonoBehaviour
 {
+    [SerializeField] public GameObject MainMenu;
+    private float _Speed = 1.5f;
+    private Vector2 m_NewPosition;
+    bool SetAnimateOptions = false;
+
     public void NewGameButtonClick(){
         Debug.Log("New");
     }
     public void OptionsButtonClick(){
         Debug.Log("Options");
+        SetAnimateOptions = true;
     }
     public void ExitButtonClick() {
         Application.Quit();
         Debug.Log("Exit");
     }
 
+    void Update(){
+        if (SetAnimateOptions) {
+            m_NewPosition = new Vector2(0.0f, -300.0f);
+            MainMenu.transform.Translate(m_NewPosition * Time.deltaTime * _Speed);
+        }
+    }
 }
