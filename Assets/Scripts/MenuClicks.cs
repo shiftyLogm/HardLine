03ebc2ac04fae374d2a6222c94d2d561440978e1
shiftyLogm <poisondroid456@gmail.com>
2024-03-14 23:21:50 -0300

    feat

--- Assets/Scripts/MenuClicks.cs
+++ Assets/Scripts/MenuClicks.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 public class MenuClicks : MonoBehaviour
 {
     private bool SetAnimateOptions = false;
@@ -8,12 +10,19 @@ public class MenuClicks : MonoBehaviour
     public RectTransform OptionsMenu;
     public GameObject ArrowTurnBack;
     public GameObject MenuOptions;
+    public Image BonfireReal;
+    public GameObject UIBlur;
+    private float speedColor;
+    private Color initialColor;
     void Start()
     {
         MainMenuRect = GetComponent<RectTransform>();
         MainMenuRect.anchoredPosition = new Vector2(0, -330.6f);
         OptionsMenu.anchoredPosition = new Vector2(0, 1054);
         ArrowTurnBack.transform.localScale = new Vector3(2, 1, 2);
+        speedColor = 10f;
+        initialColor = BonfireReal.color;
+        UIBlur.SetActive(false);
     }
 
     public void NewGameButtonClick()
@@ -33,9 +42,11 @@ public class MenuClicks : MonoBehaviour
     {
         if (SetAnimateOptions) 
         {   
+            UIBlur.SetActive(true);
             SetMenuOptions = false;
+            BonfireReal.color = Color.Lerp(BonfireReal.color, new(0, 0, 0 ,0), speedColor * Time.deltaTime);
             FunctionsMenu.AnimateVectorLerp(MainMenuRect, new (0f, -700f), 8);
-            FunctionsMenu.AnimateVectorLerp(OptionsMenu, new (0, 0), 8);
+            FunctionsMenu.AnimateVectorLerp(OptionsMenu, new (0, 0), 8);    
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -46,8 +57,11 @@ public class MenuClicks : MonoBehaviour
         if (SetAnimateOptions == false)
         {
             SetMenuOptions = true;
+            BonfireReal.color = Color.Lerp(BonfireReal.color, initialColor, speedColor * Time.deltaTime);
             FunctionsMenu.AnimateVectorLerp(MainMenuRect, new (0, -330.6f), 8);
             FunctionsMenu.AnimateVectorLerp(OptionsMenu,new (0, 1054), 8);
+            UIBlur.SetActive(false);
         }
+
     }
 }
