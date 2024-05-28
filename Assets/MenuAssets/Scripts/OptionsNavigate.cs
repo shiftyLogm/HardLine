b78b8a045f7c01bdd7028a49d269227f81a46015
shiftyLogm <poisondroid456@gmail.com>
2024-05-28 05:44:52 -0300

    feat

--- Assets/MenuAssets/Scripts/OptionsNavigate.cs
+++ Assets/MenuAssets/Scripts/OptionsNavigate.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
